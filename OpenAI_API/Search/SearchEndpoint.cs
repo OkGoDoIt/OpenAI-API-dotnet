acba79e6ddcee24c2bb998f250e0768cf1bd7a84
mike@gotmike.com <mike@gotmike.com>
2023-01-31 16:45:01 -0500

    typo in obsolete decoration;

--- OpenAI_API/Search/SearchEndpoint.cs
+++ OpenAI_API/Search/SearchEndpoint.cs
@@ -14,7 +14,7 @@ namespace OpenAI_API
     /// <summary>
     /// The API lets you do semantic search over documents. This means that you can provide a query, such as a natural language question or a statement, and find documents that answer the question or are semantically related to the statement. The “documents” can be words, sentences, paragraphs or even longer documents. For example, if you provide documents "White House", "hospital", "school" and query "the president", you’ll get a different similarity score for each document. The higher the similarity score, the more semantically similar the document is to the query (in this example, “White House” will be most similar to “the president”).
     /// </summary>
-    [Obsolete("OpenAI no long supports the Search endpoint")]
+    [Obsolete("OpenAI no longer supports the Search endpoint")]
 	public class SearchEndpoint
 	{
 		OpenAIAPI Api;
