e7d6af524fedc17cd16fb50f830850f5e82f93c6
Patrick Knäpel <patrick.knaepel@pandaliebe.de>
2023-03-02 07:57:58 +0100

    #49 IMPLEMENT IModelsEndpoint

---
+++ OpenAI_API/Model/IModelsEndpoint.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace OpenAI_API.Models
+{
+    /// <summary>
+    /// The API endpoint for querying available models
+    /// </summary>
+    public interface IModelsEndpoint
+    {
+        /// <summary>
+        /// Get details about a particular Model from the API, specifically properties such as <see cref="Model.OwnedBy"/> and permissions.
+        /// </summary>
+        /// <param name="id">The id/name of the model to get more details about</param>
+        /// <returns>Asynchronously returns the <see cref="Model"/> with all available properties</returns>
+        Task<Model> RetrieveModelDetailsAsync(string id);
+
+        /// <summary>
+        /// Get details about a particular Model from the API, specifically properties such as <see cref="Model.OwnedBy"/> and permissions.
+        /// </summary>
+        /// <param name="id">The id/name of the model to get more details about</param>
+        /// <param name="auth">Obsolete: IGNORED</param>
+        /// <returns>Asynchronously returns the <see cref="Model"/> with all available properties</returns>
+        Task<Model> RetrieveModelDetailsAsync(string id, APIAuthentication auth = null);
+
+        /// <summary>
+        /// List all models via the API
+        /// </summary>
+        /// <returns>Asynchronously returns the list of all <see cref="Model"/>s</returns>
+        Task<List<Model>> GetModelsAsync();
+    }
+}
