c81ac58f17a1f1b4fe247b79d77e52c9d31438c1
Roger <github@betechie.com>
2023-03-08 16:25:08 -0800

    Chat endpoint improvements, tests, readme updates, and an alternate Conversation class

--- OpenAI_API/Chat/ChatResult.cs
+++ OpenAI_API/Chat/ChatResult.cs
@@ -9,107 +9,131 @@ namespace OpenAI_API.Chat
 	/// Represents a result from calling the Chat API
 	/// </summary>
 	public class ChatResult : ApiResultBase
-    {
-        /// <summary>
-        /// The identifier of the result, which may be used during troubleshooting
-        /// </summary>
-        [JsonProperty("id")]
-        public string Id { get; set; }
+	{
+		/// <summary>
+		/// The identifier of the result, which may be used during troubleshooting
+		/// </summary>
+		[JsonProperty("id")]
+		public string Id { get; set; }
 
-        /// <summary>
-        /// The list of choices that the user was presented with during the chat interaction
-        /// </summary>
-        [JsonProperty("choices")]
-        public IReadOnlyList<ChatChoice> Choices { get; set; }
+		/// <summary>
+		/// The list of choices that the user was presented with during the chat interaction
+		/// </summary>
+		[JsonProperty("choices")]
+		public IReadOnlyList<ChatChoice> Choices { get; set; }
 
-        /// <summary>
-        /// The usage statistics for the chat interaction
-        /// </summary>
-        [JsonProperty("usage")]
-        public ChatUsage Usage { get; set; }
-    }
+		/// <summary>
+		/// The usage statistics for the chat interaction
+		/// </summary>
+		[JsonProperty("usage")]
+		public ChatUsage Usage { get; set; }
 
-    /// <summary>
-    /// A message received from the API, including the message text, index, and reason why the message finished.
-    /// </summary>
-    public class ChatChoice
-    {
-        /// <summary>
-        /// The index of the choice in the list of choices
-        /// </summary>
-        [JsonProperty("index")]
-        public int Index { get; set; }
+		/// <summary>
+		/// A convenience method to return the content of the message in the first choice of this response
+		/// </summary>
+		/// <returns>The content of the message, not including <see cref="ChatMessageRole"/>.</returns>
+		public override string ToString()
+		{
+			if (Choices != null && Choices.Count > 0)
+				return Choices[0].ToString();
+			else
+				return null;
+		}
+	}
 
-        /// <summary>
-        /// The message that was presented to the user as the choice
-        /// </summary>
-        [JsonProperty("message")]
-        public ChatMessage Message { get; set; }
+	/// <summary>
+	/// A message received from the API, including the message text, index, and reason why the message finished.
+	/// </summary>
+	public class ChatChoice
+	{
+		/// <summary>
+		/// The index of the choice in the list of choices
+		/// </summary>
+		[JsonProperty("index")]
+		public int Index { get; set; }
 
-        /// <summary>
-        /// The reason why the chat interaction ended after this choice was presented to the user
-        /// </summary>
-        [JsonProperty("finish_reason")]
-        public string FinishReason { get; set; }
+		/// <summary>
+		/// The message that was presented to the user as the choice
+		/// </summary>
+		[JsonProperty("message")]
+		public ChatMessage Message { get; set; }
 
-        /// <summary>
-        /// Partial message "delta" from a stream. For example, the result from <see cref="ChatEndpoint.StreamChatEnumerableAsync(ChatRequest)">StreamChatEnumerableAsync.</see>
-        /// If this result object is not from a stream, this will be null
-        /// </summary>
-        [JsonProperty("delta")]
-        public ChatMessage Delta { get; set; }
-    }
+		/// <summary>
+		/// The reason why the chat interaction ended after this choice was presented to the user
+		/// </summary>
+		[JsonProperty("finish_reason")]
+		public string FinishReason { get; set; }
 
-    /// <summary>
-    /// Chat message sent or received from the API. Includes who is speaking in the "role" and the message text in the "content"
-    /// </summary>
-    public class ChatMessage
-    {
-        /// <summary>
-        /// Constructor for a new Chat Message
-        /// </summary>
-        /// <param name="role">The role of the message, which can be "system", "assistant" or "user"</param>
-        /// <param name="content">The text to send in the message</param>
-        public ChatMessage(string role, string content)
-        {
-            this.Role = role;
-            this.Content = content;
-        }
+		/// <summary>
+		/// Partial message "delta" from a stream. For example, the result from <see cref="ChatEndpoint.StreamChatEnumerableAsync(ChatRequest)">StreamChatEnumerableAsync.</see>
+		/// If this result object is not from a stream, this will be null
+		/// </summary>
+		[JsonProperty("delta")]
+		public ChatMessage Delta { get; set; }
 
-        /// <summary>
-        /// The role of the message, which can be "system", "assistant" or "user"
-        /// </summary>
-        [JsonProperty("role")]
-        public string Role { get; set; }
+		/// <summary>
+		/// A convenience method to return the content of the message in this response
+		/// </summary>
+		/// <returns>The content of the message in this response, not including <see cref="ChatMessageRole"/>.</returns>
+		public override string ToString()
+		{
+			return Message.Content;
+		}
+	}
 
-        /// <summary>
-        /// The content of the message
-        /// </summary>
-        [JsonProperty("content")]
-        public string Content { get; set; }
-    }
+	/// <summary>
+	/// Chat message sent or received from the API. Includes who is speaking in the "role" and the message text in the "content"
+	/// </summary>
+	public class ChatMessage
+	{
+		public ChatMessage() { }
 
-    /// <summary>
-    /// How many tokens were used in this chat message.
-    /// </summary>
-    public class ChatUsage
-    {
-        /// <summary>
-        /// The number of prompt tokens used during the chat interaction
-        /// </summary>
-        [JsonProperty("prompt_tokens")]
-        public int PromptTokens { get; set; }
+		/// <summary>
+		/// Constructor for a new Chat Message
+		/// </summary>
+		/// <param name="role">The role of the message, which can be "system", "assistant" or "user"</param>
+		/// <param name="content">The text to send in the message</param>
+		public ChatMessage(ChatMessageRole role, string content)
+		{
+			this.Role = role;
+			this.Content = content;
+		}
 
-        /// <summary>
-        /// The number of completion tokens used during the chat interaction
-        /// </summary>
-        [JsonProperty("completion_tokens")]
-        public int CompletionTokens { get; set; }
+		[JsonProperty("role")]
+		internal string rawRole { get; set; }
 
-        /// <summary>
-        /// The total number of tokens used during the chat interaction
-        /// </summary>
-        [JsonProperty("total_tokens")]
-        public int TotalTokens { get; set; }
-    }
+		/// <summary>
+		/// The role of the message, which can be "system", "assistant" or "user"
+		/// </summary>
+		[JsonIgnore]
+		public ChatMessageRole Role
+		{
+			get
+			{
+				return ChatMessageRole.FromString(rawRole);
+			}
+			set
+			{
+				rawRole = value.ToString();
+			}
+		}
+
+		/// <summary>
+		/// The content of the message
+		/// </summary>
+		[JsonProperty("content")]
+		public string Content { get; set; }
+	}
+
+	/// <summary>
+	/// How many tokens were used in this chat message.
+	/// </summary>
+	public class ChatUsage : Usage
+	{
+		/// <summary>
+		/// The number of completion tokens used during the chat interaction
+		/// </summary>
+		[JsonProperty("completion_tokens")]
+		public int CompletionTokens { get; set; }
+	}
 }
