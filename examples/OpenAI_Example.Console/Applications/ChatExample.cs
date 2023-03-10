0e264ef25d800f6347f612d5399bc90b8fe633b7
Arjan Spieard | Asgard Sings! <arjan.spieard@gmail.com>
2023-03-10 15:32:12 +0100

    Chat and Completion Example added as example project.

---
+++ examples/OpenAI_Example.Console/Applications/ChatExample.cs
@@ -0,0 +1,22 @@
+﻿using OpenAI_API;
+using System;
+using System.Threading.Tasks;
+
+namespace OpenAI_Example.ConsoleApp.Applications
+{
+    internal class ChatExample : IProgram
+    {
+        public async Task RunAsync(string apiKey)
+        {
+            Console.WriteLine($"Running the {GetType()}....");
+            Console.WriteLine("Please ask a question:");
+            var api = new OpenAIAPI(apiKey);
+
+            var str = Console.ReadLine();
+            var result = await api.Chat.CreateChatCompletionAsync(str);
+
+            var reply = result.Choices[0].Message;
+            Console.WriteLine($"{reply.Role}: {reply.Content.Trim()}");
+        }
+    }
+}
---
+++ examples/OpenAI_Example.Console/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace OpenAI_Example.ConsoleApp
+{
+    internal sealed class Program
+    {
+        private static async Task Main()
+        {
+            Console.WriteLine("Welcome to the OpenAI Example program.");
+            Console.WriteLine("Please provide a valid OpenAI Api Key:");
+            var apiKey = Console.ReadLine();
+            if (String.IsNullOrWhiteSpace(apiKey))
+            {
+                throw new InvalidOperationException("Cannot authorize with OpenAI when no valid API Key is provided.");
+            }
+
+            Console.WriteLine("What do you want to do?");
+
+            Type[] types = GetExamples();
+            _ = int.TryParse(Console.ReadLine(), out var programToRun);
+
+            var instance = (IProgram?)Activator.CreateInstance(types[programToRun]);
+            if (instance is not null)
+            {
+                await instance.RunAsync(apiKey);
+            }
+
+            Console.WriteLine("Thank you for using the OpenAI Example program.");
+            Console.WriteLine("Exiting now....");
+        }
+
+        private static Type[] GetExamples()
+        {
+            var type = typeof(IProgram);
+            var types = AppDomain.CurrentDomain.GetAssemblies()
+                                               .SelectMany(s => s.GetTypes())
+                                               .Where(type.IsAssignableFrom)
+                                               .ToArray();
+
+            for (var i = 0; i < types.Length; i++)
+            {
+                var option = types[i];
+                if (option.Name == nameof(IProgram)) // Don't include the IProgram type.
+                {
+                    continue;
+                }
+                Console.WriteLine($"\t{i}){option.Name}");
+            }
+
+            return types;
+        }
+    }
+}
