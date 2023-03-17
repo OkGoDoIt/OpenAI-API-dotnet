41cc18d86189e98c7e8dc682cb3d3233ed72bdb5
mateusz.krzaczek <mateusz.krzaczek@gmail.com>
2023-03-17 16:07:47 +0100

    Fixed Issue #84

---
+++ OpenAI_Tests/ChatMessageRoleTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using OpenAI_API.Chat;
+
+namespace OpenAI_Tests
+{
+    public class ChatMessageRoleTests
+    {
+        [Test]
+        public void TestImplicitConversionNotThrowing()
+        {
+            // ReSharper disable once UnusedVariable
+            string result = ChatMessageRole.System;
+        }
+        
+        [Test]
+        public void TestImplicitConversionValue()
+        {
+            string result = ChatMessageRole.System;
+            Assert.AreEqual("system", result);
+        }
+    }
+}
