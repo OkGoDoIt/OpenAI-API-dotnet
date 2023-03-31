c1aacb9471470638e2375cba954da0c29fb511f7
Simon Saliba <s.saliba@swen.it>
2023-03-31 15:37:38 +0200

    Raiggiunto il programma di test

---
+++ TestOpenAI_API/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using OpenAI_API;
+using OpenAI_API.Models;
+
+var OPENAI_KEY = Environment.GetEnvironmentVariable("TEST_OPENAI_SECRET_KEY", EnvironmentVariableTarget.User);
+
+OpenAIAPI api = new OpenAIAPI(OPENAI_KEY);
+var at = api.AudioTranscription;
+
+var trans = await at.CreateTranscriptionAsync(file: @"c:\temp\SchoolEducationSystemEngland.mp3", model: Model.Whisper);
+
+Console.WriteLine("-------------------  test ottenuto dall'audio -------------------------");
+File.WriteAllText(@"c:\temp\SchoolEducationSystemEngland.txt", trans.Text);
+Console.WriteLine(trans.Text);
+Console.WriteLine("--------------------------- fine testo --------------------------------");
